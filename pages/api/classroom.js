dd4ce52ff71a3b5b33e2120e31b25710e9dba641
johnson-collab <johnsonjoiple@gmail.com>
2026-04-09 10:51:32 -0400

    Add API handler for classroom assignments

--- pages/api/classroom.js
+++ pages/api/classroom.js
@@ -1,1 +1,8 @@
+export default function handler(req, res) {
+  const assignments = [
+    { title: "Math Homework", subject: "Math" },
+    { title: "Science Project", subject: "Science" },
+  ];
 
+  res.status(200).json(assignments);
+}
